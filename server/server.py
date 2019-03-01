fd3d64ada7e3bcda2f19ed285aea3a23d5266cf3
Ajesh DS <ajeshd17@gmail.com>
2019-03-01 19:41:13 +0530

    Add CORS support

--- server/server.py
+++ server/server.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from admin import admin_routes
 from user import user_routes
 
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.register_blueprint(admin_routes)
 app.register_blueprint(user_routes)
