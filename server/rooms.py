eeb137a29125dfb633405ca415383ec490b56009
Ajesh DS <ajeshd17@gmail.com>
2019-03-14 17:44:45 +0530

    Restrict creation of rooms with duplicate names and numbers

--- server/rooms.py
+++ server/rooms.py
@@ -12,12 +12,17 @@ room_routes = Blueprint('room_routes', __name__)
 @admin_login_required
 def create():
     try:
+        admin = Admin.objects(id=g.admin['id']).first()
         name = request.json['name']
         roomnumber = request.json['roomnumber']
         capacity = request.json['capacity']
+        for r in admin.rooms:
+            if r.name == name:
+                raise Exception('You have already created a room with name as \'{}\''.format(name))
+            if r.roomnumber == roomnumber:
+                raise Exception('You already have a room with the same room number. ({})'.format(r.name))
         room = Room(name=name, roomnumber=roomnumber, capacity=capacity)
         room.save()
-        admin = Admin.objects(id=g.admin['id']).first()
         admin.rooms.append(room)
         admin.save()
         return room.to_json(), 200
